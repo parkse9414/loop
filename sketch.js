5e2edd48d0d90d85ac2c3ffcdec189a04ee1111a
박세원 <parkse9414@naver.com>
2014-09-11 15:28:53 +0900

    ㄴㅇㄹㄴㅇㄹ

--- sketch.js
+++ sketch.js
@@ -7,6 +7,7 @@ function setup() {
 
   stroke(255,0,0);
 
+/*
   var x;
 
   x = 1;
@@ -17,4 +18,11 @@ function setup() {
   
   x = 3;
   ellipse(x * 300,100,50,50);
+ 
+ */
+
+ for(var x = 1; x <=3; x++)
+  {
+	ellipse(x * 100,100,50,50);
+  }
 }
