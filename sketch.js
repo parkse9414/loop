d2e02fc5196a37e54087d6c42660c5f8cc770ed6
박세원 <parkse9414@naver.com>
2014-09-11 16:03:43 +0900

    ㅇㄴㄹㅇㄴㄹㅇㄴㄹ

--- sketch.js
+++ sketch.js
@@ -1,7 +1,7 @@
 function setup() {
   createCanvas(640, 480);
 
-  background(179, 212, 101);
+
  
   noFill();
 
@@ -10,28 +10,14 @@ function setup() {
 
 function draw() {
 
-
-/*
-  var x;
-
-  x = 1;
-  ellipse(x * 100,100,50,50);
+  background(179, 212, 101);
   
-  x = 2;
-  ellipse(x * 200,100,50,50);
-  
-  x = 3;
-  ellipse(x * 300,100,50,50);
- 
- */
-
-
 for (var x = 1; x <= 8; x++)
   {
   ellipse(x * 70, x * 20 + 100, 40, 50);
   }
 
-var size = Noth.abs(mouseX - 328)
+var size = Math.abs(mouseX - 328)
 
 ellipse(320, 240, mouseX, mouseY);
 
