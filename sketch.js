8e0f8122a0b98ad722b1060566dd86e0ad25a372
박세원 <parkse9414@naver.com>
2014-09-11 15:09:08 +0900

    ㅇㅇㅇㅇ

---
+++ sketch.js
@@ -0,0 +1,20 @@
+function setup() {
+  createCanvas(640, 480);
+
+  background(179, 212, 101);
+ 
+  noFill();
+
+  stroke(255,0,0);
+
+  var x;
+
+  x=100;
+  ellipse(100,100,50,50);
+  
+  x=200;
+  ellipse(200,100,50,50);
+  
+  x=300;
+  ellipse(300,100,50,50);
+}
